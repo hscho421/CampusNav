ca441205a9e6c8768973b1f27557fb4243b7a8c1
Hyunseok Cho <dant0421@gmail.com>
2024-06-10 23:20:48 +0900

    added table

--- frontend/src/components/ScheduleTable/ScheduleTable.jsx
+++ frontend/src/components/ScheduleTable/ScheduleTable.jsx
@@ -1,76 +1,134 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import './ScheduleTable.css'
+import './ScheduleTable.css';
 
 const ScheduleTable = () => {
     const { t } = useTranslation();
+    const [selectedWeekday, setSelectedWeekday] = useState('');
     const [courses, setCourses] = useState([
-        { courseName: '', location: '', startTime: '', endTime: '' }
+        { weekday: '', courseName: '', location: '', startHour: '', startMinute: '', endHour: '', endMinute: '' }
     ]);
 
     const handleChange = (index, event) => {
         const { name, value } = event.target;
         const updatedCourses = [...courses];
         updatedCourses[index][name] = value;
+        updatedCourses[index].weekday = selectedWeekday; // Set the weekday based on selected button
         setCourses(updatedCourses);
 
         if (index === courses.length - 1 && value !== '') {
             const allFieldsFilled = Object.values(updatedCourses[index]).every(field => field !== '');
             if (allFieldsFilled) {
-              setCourses([...courses, { courseName: '', location: '', startTime: '', endTime: '' }]);
+                setCourses([...courses, { weekday: '', courseName: '', location: '', startHour: '', startMinute: '', endHour: '', endMinute: '' }]);
             }
         }
     };
 
+    const handleDayClick = (day) => {
+        setSelectedWeekday(day);
+    };
+
+    const generateOptions = (range) => {
+        return Array.from({ length: range }, (v, k) => k).map((num) => (
+            <option key={num} value={num < 10 ? `0${num}` : num}>{num < 10 ? `0${num}` : num}</option>
+        ));
+    };
+
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>{t('courseName')}</th>
-                    <th>{t('location')}</th>
-                    <th>{t('startTime')}</th>
-                    <th>{t('endTime')}</th>
-                </tr>
-            </thead>
-            <tbody>
-            {courses.map((course, index) => (
-                <tr key={index}>
-                    <td>
-                        <input
-                            type="text"
-                            name="courseName"
-                            value={course.courseName}
-                            onChange={(event) => handleChange(index, event)}
-                        />
-                    </td>
-                    <td>
-                        <input
-                            type="text"
-                            name="location"
-                            value={course.location}
-                            onChange={(event) => handleChange(index, event)}
-                        />
-                    </td>
-                    <td>
-                        <input
-                            type="time"
-                            name="startTime"
-                            value={course.startTime}
-                            onChange={(event) => handleChange(index, event)}
-                        />
-                    </td>
-                    <td>
-                        <input
-                            type="time"
-                            name="endTime"
-                            value={course.endTime}
-                            onChange={(event) => handleChange(index, event)}
-                        />
-                    </td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
+        <div>
+            <div className="weekday-buttons">
+                {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map((day) => (
+                    <button
+                        key={day}
+                        type="button"
+                        className={selectedWeekday === day ? 'selected-day' : ''}
+                        onClick={() => handleDayClick(day)}
+                    >
+                        {t(day)}
+                    </button>
+                ))}
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>{t('courseName')}</th>
+                        <th>{t('location')}</th>
+                        <th>{t('startTime')}</th>
+                        <th>{t('endTime')}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {courses.map((course, index) => (
+                        <tr key={index}>
+                            <td>
+                                <input
+                                    type="text"
+                                    name="courseName"
+                                    value={course.courseName}
+                                    onChange={(event) => handleChange(index, event)}
+                                    className="invisible-input"
+                                />
+                            </td>
+                            <td>
+                                <input
+                                    type="text"
+                                    name="location"
+                                    value={course.location}
+                                    onChange={(event) => handleChange(index, event)}
+                                    className="invisible-input"
+                                />
+                            </td>
+                            <td>
+                                <div className="time-select-wrapper">
+                                    <select
+                                        name="startHour"
+                                        value={course.startHour}
+                                        onChange={(event) => handleChange(index, event)}
+                                        className="time-select"
+                                    >
+                                        <option value="">{t('--')}</option>
+                                        {generateOptions(24)}
+                                    </select>
+                                    :
+                                    <select
+                                        name="startMinute"
+                                        value={course.startMinute}
+                                        onChange={(event) => handleChange(index, event)}
+                                        className="time-select"
+                                    >
+                                        <option value="">{t('--')}</option>
+                                        {generateOptions(60)}
+                                    </select>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="time-select-wrapper">
+                                    <select
+                                        name="endHour"
+                                        value={course.endHour}
+                                        onChange={(event) => handleChange(index, event)}
+                                        className="time-select"
+                                    >
+                                        <option value="">{t('--')}</option>
+                                        {generateOptions(24)}
+                                    </select>
+                                    :
+                                    <select
+                                        name="endMinute"
+                                        value={course.endMinute}
+                                        onChange={(event) => handleChange(index, event)}
+                                        className="time-select"
+                                    >
+                                        <option value="">{t('--')}</option>
+                                        {generateOptions(60)}
+                                    </select>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
