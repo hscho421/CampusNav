cad022de7343b1089b55e86059862b69169449c6
Hyunseok Cho <dant0421@gmail.com>
2024-06-15 20:04:43 +0900

    css changes

--- frontend/src/components/TimeTable/TimeTable.jsx
+++ frontend/src/components/TimeTable/TimeTable.jsx
@@ -6,13 +6,21 @@ const TimeTable = ({ courses }) => {
 
   const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
+  const sortCourses = (courses) => {
+    return courses.sort((a, b) => {
+      const timeA = parseInt(a.startHour) * 60 + parseInt(a.startMinute);
+      const timeB = parseInt(b.startHour) * 60 + parseInt(b.startMinute);
+      return timeA - timeB;
+    }); 
+  };
+
   return (
     <div className='time-table-container'>
       <div className="time-table">
         {days.map(day => (
           <div key={day} className={`${day}-column`}>
             <h3 className='day'>{day}</h3>
-            {Array.isArray(courses) && courses.filter(course => course.weekday === day).map((course, index) => (
+            {Array.isArray(courses) && sortCourses(courses.filter(course => course.weekday === day)).map((course, index) => (
               <div key={index} className="course">
                 <p>{course.courseName} ({course.location})</p>
                 <p>{course.startHour}:{course.startMinute} - {course.endHour}:{course.endMinute}</p>
